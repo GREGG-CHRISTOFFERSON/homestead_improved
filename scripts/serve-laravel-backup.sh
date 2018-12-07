3ca184c25646dd77c4ad1e4a4e3364e617e9a654
Gregg Christofferson <gechristofferson@gmail.com>
2018-12-07 15:07:05 -0800

    backed up and edited serve-laravel.sh so that ssl certificates are generated with a SAN extension and are signed by self-signed Certification Authority

--- scripts/serve-laravel-backup.sh
+++ scripts/serve-laravel-backup.sh
@@ -7,44 +7,17 @@ PATH_KEY="${PATH_SSL}/${1}.key"
 PATH_CSR="${PATH_SSL}/${1}.csr"
 PATH_CRT="${PATH_SSL}/${1}.crt"
 
-PATH_SAN_EXTENSION="${PATH_SSL}/${1}.san"
-
-PATH_CA_ROOT_CRT="${PATH_SSL}/homestead_root_ca.crt"
-PATH_CA_ROOT_KEY="${PATH_SSL}/homestead_root_ca.key"
-PATH_CA_ROOT_SRL="${PATH_SSL}/homestead_root_ca.srl"
-
-PATH_CERT_EXPORT_DIR="/home/vagrant/Code/cert"
-PATH_CERT_EXPORT="${PATH_CERT_EXPORT_DIR}/homestead_root_ca.crt"
-
-if [ ! -f $PATH_CA_ROOT_CRT ] || [ ! -f $PATH_CA_ROOT_KEY ]
-then
-  openssl req -x509 -nodes -newkey rsa:4096 -keyout "$PATH_CA_ROOT_KEY" -out "$PATH_CA_ROOT_CRT" -days 365 -subj "/C=UK/O=Vagrant Homestead Improved/CN=Vagrant Homestead Improved Root CA" 2>/dev/null
-fi
-
-if [ ! -f $PATH_SAN_EXTENSION ]
-then
-  printf "[SAN]\nsubjectAltName=DNS:${1},DNS:www.${1}\n" > $PATH_SAN_EXTENSION
-fi
-
 if [ ! -f $PATH_KEY ] || [ ! -f $PATH_CSR ] || [ ! -f $PATH_CRT ]
 then
   openssl genrsa -out "$PATH_KEY" 2048 2>/dev/null
-  openssl req -new -key "$PATH_KEY" -out "$PATH_CSR" -subj "/CN=$1" 2>/dev/null
-  openssl x509 -req -days 365 -extfile "$PATH_SAN_EXTENSION" -extensions "SAN" -CAcreateserial -CAserial "$PATH_CA_ROOT_SRL" -CAkey "$PATH_CA_ROOT_KEY" -CA "$PATH_CA_ROOT_CRT" -in "$PATH_CSR" -out "$PATH_CRT" 2>/dev/null
+  openssl req -new -key "$PATH_KEY" -out "$PATH_CSR" -subj "/CN=$1/O=Vagrant/C=UK" 2>/dev/null
+  openssl x509 -req -days 365 -in "$PATH_CSR" -signkey "$PATH_KEY" -out "$PATH_CRT" 2>/dev/null
 fi
-
-if [ ! -d $PATH_CERT_EXPORT_DIR ]
-then
-  mkdir -p $PATH_CERT_EXPORT_DIR
-fi
-
-cp -u $PATH_CA_ROOT_CRT $PATH_CERT_EXPORT
-
 
 block="server {
     listen ${3:-80};
     listen ${4:-443} ssl http2;
-    server_name $1 www.$1;
+    server_name $1;
     root \"$2\";
 
     index index.html index.htm index.php;
@@ -67,7 +40,7 @@ block="server {
 
     location ~ \.php$ {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/var/run/php/php7.1-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
         fastcgi_index index.php;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
